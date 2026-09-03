4f4f2069feba0342a28ad986a7747644f5923a2a
Thay Brown <ThayBrown22@gmail.com>
2026-09-03 10:28:08 -0400

    Answer to keep hydrated CodeWars question

---
+++ 8kyu/keep-hydrated/ans.js
@@ -0,0 +1,10 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+function litres(time) {
+  let l = Math.floor(time * .5)
+  return l
+  }
